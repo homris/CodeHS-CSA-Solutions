59fa4bd824bd3e4149329085fcb6b7b01d7809de
homris <mhomris2006@gmail.com>
2023-12-04 09:40:38 +0300

    Added 5.1 files

---
+++ Unit-5 Writings Classes/5.1 Writing Classes/5.1.5 Access for Employee Class/Employee.java
@@ -0,0 +1,14 @@
+public class Employee
+{
+    private String name;
+    private String lastName;
+    private double salary;
+    private int hours;
+    
+    public Employee(String theName, String theLastName, double theSalary, int theHours){
+        theName = name;
+        theLastName = lastName;
+        theSalary = salary;
+        theHours = hours;
+    }
+}
