01d9ff974f5c75c164fcad1282a6defc445a6cc6
homris <mhomris2006@gmail.com>
2024-01-11 23:46:10 +0300

    Added 5.8 files

---
+++ Unit-5 Writings Classes/5.8 Scope and Access/5.8.7 Scope/ScopeTester.java
@@ -0,0 +1,9 @@
+public class ScopeTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        Scope printScope = new Scope();
+        printScope.printScope();
+    }
+}
